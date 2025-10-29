d913a2c7ff2696a45af5734397dff65a85e5d054
Avinash Karri <129477074+AvinashDigitals709@users.noreply.github.com>
2025-10-29 11:30:57 +0530

    Add add function to calculate sum in lambda_handler

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,11 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+    def add(a, b):
+        return a + b
+        
+    print("Sum:", add(5, 7))
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
